4dae5d85710a34fe8896ebfec840382ee043329f
JamesDBartlett3 <github@jdbartlett.net>
2023-07-17 18:52:19 -0500

    updated install_jupyterlab.sh

--- install_jupyterlab.sh
+++ install_jupyterlab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo apt install python3 python3-pip dex
+sudo apt install python3 python3-pip
 pip install jupyterlab matplotlib numpy pandas seaborn squarify
+echo -e '\n# set PATH so it includes user private bin if it exists\nif [ -d "$HOME/.local/bin" ] ; then\n    PATH="$HOME/.local/bin:$PATH"\nfi' >> ~/.profile
 echo ". ./.profile;jupyter lab" >> ~/run_jupyterlab.sh
 chmod +x ~/run_jupyterlab.sh
-dex -c ~/run_jupyterlab.sh /usr/share/applications/JupyterLab
-
+~/run_jupyterlab.sh
