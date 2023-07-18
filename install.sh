ebed86cb4bee8dcac7418d73fa17bb5d95365d84
JamesDBartlett3 <github@jdbartlett.net>
2023-07-18 18:14:40 -0500

    modified install.sh to tell the user what it's doing and ask if they want to run JupyterLab when installation is finished

--- install.sh
+++ install.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
-sudo apt install python3 python3-pip
-pip install jupyterlab matplotlib numpy pandas seaborn squarify
-echo -e '\n# set PATH so it includes user private bin if it exists\nif [ -d "$HOME/.local/bin" ] ; then\n    PATH="$HOME/.local/bin:$PATH"\nfi' >> ~/.profile
-echo ". ./.profile;jupyter lab" >> ~/run_jupyterlab.sh
-chmod +x ~/run_jupyterlab.sh
-~/run_jupyterlab.sh
+echo -e "########################################\nWelcome to Data Analysis and Visualization at Tech Camp!\n########################################"
+echo -e "\nInstalling software packages for Python, Git, and their dependencies..."
+sudo apt install python3 python3-pip git
+
+echo -e "\nInstalling JupyterLab, Matplotlib, NumPy, Pandas, Seaborn, and Squarify (Python libraries made for Data Analysis and Visualization)..."
+pip install jupyter jupyterlab matplotlib numpy pandas seaborn squarify
+
+echo -e "\nAdding \"$HOME/.local/bin\" (location where Jupyter is installed) to PATH environment variable for $USER (current user)..."
+echo -e '\n# set PATH so it includes user private bin if it exists\nif [ -d "$HOME/.local/bin" ] ; then\n    PATH="$HOME/.local/bin:$PATH"\nfi' >> ~/.profile2
+
+echo -e "\nCloning TechCamp_DataViz repository...\n  From: https://GitHub.com/likeawednesday/TechCamp_DataViz\n  To: $HOME/TechCamp_DataViz"
+git clone https://github.com/likeawednesday/TechCamp_DataViz.git ~/TechCamp_DataViz2
+
+echo -e "\nSetting execution permission on $HOME/TechCamp_DataViz/run_jupyterlab.sh script file..."
+chmod +x ~/TechCamp_DataViz2/run_jupyterlab.sh
+
+echo -e "\n####################\nInstallation complete!\n####################\n" 
+read -p "Run JupyterLab now? (Y/N)" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+	~/TechCamp_DataViz2/run_jupyterlab.sh
+fi
